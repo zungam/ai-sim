3fba3fd0ef737bdf4c2c19abd0d0d439f35005de
lightbits <simenh.jobb@gmail.com>
2015-12-29 14:53:14 +0100

    Added easier run script

---
+++ build/run.bat
@@ -0,0 +1,13 @@
+@echo off
+if not defined DevEnvDir (
+    call "C:\Applications\vs2012\VC\vcvarsall.bat"
+)
+set CF=-Zi -nologo -Oi -Od -WX -W4 -wd4100 -fp:fast /MD
+set LF=-subsystem:console -incremental:no -debug SDL2.lib SDL2main.lib opengl32.lib
+
+cl %CF% -I../lib/sdl/include ../platform.cpp /link %LF% -out:sim.exe
+
+cl -nologo ../ai2.cpp /link -out:ai.exe
+
+start sim.exe
+ai.exe
